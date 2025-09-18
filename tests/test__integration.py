b56ab531f5c88b60685f1248aea15f59c3bd65e9
nomutin <nomura0508@icloud.com>
2025-09-18 21:57:47 +0900

    refactor: 整理

--- tests/test__integration.py
+++ tests/test__integration.py
@@ -1,1 +1,19 @@
+from pathlib import Path
 
+import pytest
+
+import voice_embedding
+from voice_embedding import OnnxFeatureEncoderProvider, S3Provider
+
+
+@pytest.mark.integration
+def test__encode_with_model(mock_s3_provider: S3Provider) -> None:
+    model_path = Path("model.onnx")
+    encoder_provider = OnnxFeatureEncoderProvider(model_path=model_path)
+    embedding_list = voice_embedding.encode(
+        s3_provider=mock_s3_provider,
+        encoder_provider=encoder_provider,
+        bucket="test-bucket",
+        key="test-key",
+    )
+    assert len(embedding_list) == 256
